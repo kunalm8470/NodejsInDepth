d32318f1f5612f941971048785df61af6ed46efb
Kunal Mukherjee <kunalm.8470@outlook.com>
2023-04-13 22:26:41 +0530

    Added building rest apis using http and https module

---
+++ 4. Built In Modules/http-module/index.js
@@ -0,0 +1,94 @@
+const { createServer } = require('http');
+
+const posts = [
+    {
+        id: 1,
+        name: 'John Doe',
+        age: 10
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        age: 10
+    }
+];
+
+const server = createServer((req, res) => {
+    const { url, method } = req;
+
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        res.write(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <title>Page Title</title>
+            </head>
+            <body>
+            
+            <h1>Root page</h1>
+            
+            </body>
+            </html>
+        `);
+
+        res.end();
+    }
+
+    else if (url === '/posts' && method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        res.write(JSON.stringify(posts, null, 4));
+
+        res.end();
+    }
+
+    else if (url === '/posts' && method === 'POST') {
+        
+        let requestBody = '';
+        req.on('data', (chunk) => {
+            requestBody += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const requestObj = JSON.parse(requestBody);
+
+            posts.push(requestObj);
+
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+
+            res.write(JSON.stringify(requestObj, null, 4));
+    
+            res.end();
+        });
+    }
+
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+
+        res.write(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>Page Title</title>
+        </head>
+        <body>
+        
+        <h1 style="color: red;">Not found page</h1>
+        
+        </body>
+        </html>
+        `);
+
+        res.end();
+    }
+});
+
+const port = 3000;
+
+// 127.0.0.1 -> IPv4
+// ::1 -> IPv6
+server.listen(port, () => {
+    console.log(`Server started listening on port: ${port}`);
+});
